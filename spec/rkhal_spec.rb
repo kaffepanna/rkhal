6a8bc3ff5e301277acc7c4b049a21d24678e0d37
Patrik Pettersson <patrik.pettersson@ericsson.com>
2015-06-08 09:50:23 +0200

    created gem

---
+++ spec/rkhal_spec.rb
@@ -0,0 +1,11 @@
+require 'spec_helper'
+
+describe Rkhal do
+  it 'has a version number' do
+    expect(Rkhal::VERSION).not_to be nil
+  end
+
+  it 'does something useful' do
+    expect(false).to eq(true)
+  end
+end
